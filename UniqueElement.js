3c907d7c6363a7cc1c16662e6d2f4b4c3c184551
Khachatryan-01 <khachatryantyom02@gmail.com>
2024-06-15 14:15:34 +0400

    UniqueElements_Task

--- UniqueElement.js
+++ UniqueElement.js
@@ -1,1 +1,28 @@
-console.log('Unique');
+let name = 'hello';
+
+function uniqueElement(name) {
+
+    const array = name.trim().toLowerCase().split('');
+    const obj = {}
+    const uniqueElements = []
+    
+    for (let i = 0; i < array.length; i++) {
+        let num = 1
+        if (obj[array[i]] >= 1) {
+            obj[array[i]]++
+        }
+        else {
+            obj[array[i]] = num
+        }
+    }
+
+    for (let i in obj) {
+        if (obj[i] === 1) {
+            uniqueElements.push(i)
+        }
+    }
+
+    return uniqueElements
+}
+
+console.log(uniqueElement(name));
